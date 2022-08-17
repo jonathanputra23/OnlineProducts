297028d4d156548f20c64c1cf470fa92231882a2
jonathanputra23 <jonathanputra23@gmail.com>
2022-08-18 02:32:06 +0700

    Change allowed host in settings.py

--- OnlineProducts/OnlineProducts/settings.py
+++ OnlineProducts/OnlineProducts/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-#%dewe8b-!+lecl)!^7-96c5#i@t)u3her*e&phuc*^uq9-8ze
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['django-onlineproduct.herokuapp.com']
 
 
 # Application definition
